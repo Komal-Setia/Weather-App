13a52736412f6af31932f4727f2268bbe5c45281
Komal <87696831+Komal-Setia@users.noreply.github.com>
2021-12-29 11:45:25 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let weather={
-    apikey:"81c14f703d9be4eae4d1c953ec990ad2",
+    apikey:"YOUR API KEY",
     fetchWeather: function(city){
         fetch(
        "https://api.openweathermap.org/data/2.5/weather?q="
